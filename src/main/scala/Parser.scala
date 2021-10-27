66c0ab67b16e6130d13ccbe5f560140d188dab03
Nathan Kleyn <nathan@nathankleyn.com>
2021-10-27 21:21:16 +0000

    [WIP] Add collections and functions

--- src/main/scala/Parser.scala
+++ src/main/scala/Parser.scala
@@ -3,6 +3,24 @@ package proclang
 import cats.parse.{Numbers, Parser => P, Parser0 => P0}
 
 object Parsers {
+  type Primative = Boolean | String | Long
+  type Identifier = String
+
+  // FIXME: Currently getting a recursive definition error if including self
+  // types within these collections - see https://github.com/lampepfl/dotty/issues/10136
+  // Needs resolving but for now collections will be flat to make progress
+  type Struct = Map[Identifier, Primative | Fn]
+  type Sequence = List[Primative | Fn]
+  type Collection = Sequence | Struct
+
+  // FIXME: Implement body type
+  case class Fn(argDefs: Struct, body: Any) {
+    // FIXME: Implement return type
+    def run(args: Struct): Any = {
+      // FIXME: Implement calling of function
+    }
+  }
+
   val whitespace: P[Unit] = P.charIn(" \t\r\n").void
   val whitespaces0: P0[Unit] = whitespace.rep0.void
 
@@ -18,5 +36,12 @@ object Parsers {
 
   val number: P[Long] = Numbers.digits.map(_.toLong)
 
-  val primatives: P0[Boolean | String | Long] = P.oneOf0(boolean :: string :: number :: Nil)
+  val primatives: P0[Primative] = P.oneOf0(boolean :: string :: number :: Nil)
+
+  val map: P[Map[String, Primative]] =
+    P.char('{') *>
+      whitespaces0 *>
+      P.repAs(mapValue) <*
+      whitespaces0 <*
+      P.char('}')
 }
